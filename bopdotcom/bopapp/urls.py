41b09a0cee7a6d01b7caca77f99c76b300d5deb8
wezleysherman <wezleysherman@yahoo.com>
2018-04-17 10:52:21 -0700

    Working site

--- bopdotcom/bopapp/urls.py
+++ bopdotcom/bopapp/urls.py
@@ -6,5 +6,8 @@ from django.conf import settings
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('bop', views.bop_someone, name='bopsomeone'),
-	path('ajax/bop_someone/', views.bop_ajax, name='bop_ajax')
+	path('ajax/bop_someone/', views.bop_ajax, name='bop_ajax'),
+	path('userpage/<int:u_id>/', views.user_page, name='userpage'),
+	path('groups', views.groups, name='groups'),
+	path('groups/<int:g_id>/', views.group_page, name='group_info')
 ]
