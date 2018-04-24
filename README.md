c00cafd60701f18a9b2537bea0594fb6f954e9da
wezleysherman <wezleysherman@yahoo.com>
2018-04-23 20:21:11 -0700

    final changes

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
 # Bop-Dot-Com
 SER322 Group Project 
 
+Contributors:
+- Wezley Sherman
+- Maguire Gillen
+- Zachary Christensen
+- Alonna Hellinger
+- The various authors we referenced to create this awesome site.
+
+
 Dependencies:
 
-- Python3
+- Python3 (Python 2 will not work)
 - pip
 - Pillow
+- Django 2.0
+
+
+To run the server:
+- Naviate to the bopdotcom directory
+- Type: 'python manage.py runserver' (Make sure you are using Python3 or higher!)
+
+To access shell/database:
+- Naviate to the bopdotcome directory
+- Type: 'python manage.py shell' (Make sure you are using Python3 or higher!)
 
 Naviate to bopdotcom directory and use 'python manage.py runserver' to start the site.
--- bopdotcom/bopapp/static/js/csfr.js
+++ bopdotcom/bopapp/static/js/csfr.js
@@ -1,11 +1,18 @@
 /* CSFR Code utilized from:
+ *
  * https://stackoverflow.com/questions/19333098/403-forbidden-error-when-making-an-ajax-post-request-in-django-framework
- * Credit: Fivef
- * We do not claim ownership of the following code.
+ *
+ * CREDIT: Fivef
+ *
+ * We DO NOT claim ownership of the following code.
+ * Fivef wrote this code -- not us.
+ * 
  *
  * CSFR Token is used for ajax calls (Communicating frontend with the backend).
  * This code utilizes jquery.cookie library to generate a CSRF token to allow POST access.
  * This code is very generic and is utilized in any Django web app that utilizes ajax calls.
+ *
+ * All credit goes to Fivef for the following snippet:
  */
 var csrftoken = $.cookie('csrftoken');
 
